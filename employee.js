3fc0cc6adba63d0aaacc2277ed7142bb10585e80
John <jjpyae@gmail.com>
2023-05-08 22:15:01 -0700

    Dynamically added employees to Manager's employee list

--- employee.js
+++ employee.js
@@ -7,6 +7,9 @@ class Employee {
       this.manager = null;
     } else {
       this.manager = manager;
+      //when manager is passed on new Employee instance, that manager's addEmployee method should be called from Employee's constructor
+      //We want to add the whole instance by passing it into that method.
+      manager.addEmployee(this);
     }
 
   }
